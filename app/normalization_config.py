f25e0a38851820d108bd5345bf3a503c8a9fa35e
Closertech001 <danielajayi842@gmail.com>
2025-06-21 01:04:55 +0100

    Create normalization_config.py

---
+++ app/normalization_config.py
@@ -0,0 +1,61 @@
+VERB_CONJUGATIONS = {
+    "does": "do", "has": "have", "was": "be", "were": "be",
+    "did": "do", "are": "be", "is": "be", "studies": "study",
+    "teaches": "teach", "offers": "offer", "requires": "require"
+}
+
+PLURAL_MAP = {
+    "courses": "course", "subjects": "subject", "lecturers": "lecturer",
+    "departments": "department", "faculties": "faculty", "fees": "fee",
+    "requirements": "requirement", "books": "book", "students": "student"
+}
+
+ABBREVIATIONS = {
+    "u": "you", "r": "are", "ur": "your", "cn": "can", "cud": "could",
+    "shud": "should", "wud": "would", "abt": "about", "bcz": "because",
+    "plz": "please", "pls": "please", "tmrw": "tomorrow", "wat": "what",
+    "wats": "what is", "info": "information", "yr": "year", "sem": "semester",
+    "admsn": "admission", "clg": "college", "sch": "school", "uni": "university",
+    "cresnt": "crescent", "l": "level", "d": "the", "msg": "message",
+    "idk": "i don't know", "imo": "in my opinion", "asap": "as soon as possible",
+    "dept": "department", "reg": "registration", "fee": "fees", "pg": "postgraduate",
+    "app": "application", "req": "requirement", "nd": "national diploma",
+    "a-level": "advanced level", "alevel": "advanced level", "2nd": "second",
+    "1st": "first", "nxt": "next", "prev": "previous", "exp": "experience",
+    "csc": "department of computer science", "mass comm": "department of mass communication",
+    "law": "department of law", "acc": "department of accounting"
+}
+
+SYNONYMS = {
+    "lecturers": "academic staff", "professors": "academic staff",
+    "teachers": "academic staff", "instructors": "academic staff",
+    "tutors": "academic staff", "staff members": "staff",
+    "head": "dean", "hod": "head of department", "dept": "department",
+    "school": "university", "college": "faculty", "course": "subject",
+    "class": "course", "subject": "course", "unit": "credit",
+    "credit unit": "unit", "course load": "unit", "non teaching": "non-academic",
+    "admin worker": "non-academic staff", "support staff": "non-academic staff",
+    "clerk": "non-academic staff", "receptionist": "non-academic staff",
+    "secretary": "non-academic staff", "tech staff": "technical staff",
+    "hostel": "accommodation", "lodging": "accommodation", "room": "accommodation",
+    "school fees": "tuition", "acceptance fee": "admission fee", "fees": "tuition",
+    "enrol": "apply", "join": "apply", "sign up": "apply", "admit": "apply",
+    "requirement": "criteria", "conditions": "criteria", "needed": "required",
+    "needed for": "required for", "who handles": "who manages"
+}
+
+DEPARTMENT_ALIASES = {
+    "anatomy": ["ana", "dept of anatomy", "anatomy dept"],
+    "accounting": ["acc", "dept of accounting", "accounting dept"],
+    "architecture": ["arch", "dept of architecture", "architecture dept"],
+    "computer science": ["cs", "csc", "dept of computer science", "computer science dept"],
+    "law": ["bacolaw", "dept of law", "law dept"],
+    "nursing": ["nur", "dept of nursing", "nursing dept"],
+    "physiology": ["phy", "dept of physiology", "physiology dept"],
+    "mass communication": ["mass comm", "dept of mass communication", "mass comm dept"],
+    "economics": ["eco", "dept of economics", "economics dept"],
+    "business administration": ["bus admin", "dept of business admin", "business admin dept"],
+    "biochemistry": ["bch", "dept of biochemistry", "biochemistry dept"],
+    "microbiology": ["mcb", "dept of microbiology", "microbiology dept"],
+    "political science": ["pol sci", "dept of political science", "political science dept"]
+}
