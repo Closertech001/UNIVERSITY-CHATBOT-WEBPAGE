cf471db2d75f518e95d09a14e5bd2b606a310bfc
Closertech001 <danielajayi842@gmail.com>
2025-07-02 09:21:24 +0100

    Update web.py

--- web.py
+++ web.py
@@ -69,18 +69,37 @@ def detect_sentiment(text):
     return "neutral"
 
 ABBREVIATIONS = {
-    "u": "you", "r": "are", "ur": "your", "cn": "can", "pls": "please", "asap": "as soon as possible"
+    "u": "you", "r": "are", "ur": "your", "cn": "can", "cud": "could",
+    "shud": "should", "wud": "would", "abt": "about", "bcz": "because",
+    "plz": "please", "pls": "please", "tmrw": "tomorrow", "wat": "what",
+    "wats": "what is", "info": "information", "yr": "year", "sem": "semester",
+    "admsn": "admission", "clg": "college", "sch": "school", "uni": "university",
+    "cresnt": "crescent", "l": "level", "d": "the", "msg": "message",
+    "idk": "i don't know", "imo": "in my opinion", "asap": "as soon as possible",
+    "dept": "department", "reg": "registration", "fee": "fees", "pg": "postgraduate",
+    "app": "application", "req": "requirement", "nd": "national diploma",
+    "a-level": "advanced level", "alevel": "advanced level", "2nd": "second",
+    "1st": "first", "nxt": "next", "prev": "previous", "exp": "experience",
+    "CSC": "department of Computer Science", "Mass comm": "department of Mass Communication",
+    "law": "department of law", "Acc": "department of Accounting"
 }
 
 SYNONYMS = {
-    "school fees": "tuition",
-    "hostel": "accommodation",
-    "it office": "ict department",
-    "lecturers": "academic staff",
-    "non-academic staff": "administrative staff",
-    "courses": "programs",
-    "bio": "biochemistry",
-    "comp sci": "computer science"
+    "lecturers": "academic staff", "professors": "academic staff",
+    "teachers": "academic staff", "instructors": "academic staff",
+    "tutors": "academic staff", "staff members": "staff",
+    "head": "dean", "hod": "head of department", "dept": "department",
+    "school": "university", "college": "faculty", "course": "subject",
+    "class": "course", "subject": "course", "unit": "credit",
+    "credit unit": "unit", "course load": "unit", "non teaching": "non-academic",
+    "admin worker": "non-academic staff", "support staff": "non-academic staff",
+    "clerk": "non-academic staff", "receptionist": "non-academic staff",
+    "secretary": "non-academic staff", "tech staff": "technical staff",
+    "hostel": "accommodation", "lodging": "accommodation", "room": "accommodation",
+    "school fees": "tuition", "acceptance fee": "admission fee", "fees": "tuition",
+    "enrol": "apply", "join": "apply", "sign up": "apply", "admit": "apply",
+    "requirement": "criteria", "conditions": "criteria", "needed": "required",
+    "needed for": "required for", "who handles": "who manages"
 }
 
 class AIService:
